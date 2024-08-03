a04e4c0cedb193892288cb2a8b31810e3f7e9f06
Midlaj Marutha <midlajmarutha4@gmail.com>
2024-08-03 20:10:41 +0530

    added utils.py

---
+++ .ipynb_checkpoints/utils-checkpoint.py
@@ -0,0 +1,33 @@
+import numpy as np
+
+def dictionary_to_vector(parameters):
+    """
+    converts parameter dictionary to a single vector
+
+    Arguments:
+    parameters: dictionary of parameters
+
+    Return:
+    vector: vector of model parameters
+    """
+    vector = []
+    for i in parameters:
+        reshaped = np.reshape(parameters[i], parameters[i].shape[0]*parameters[i].shape[1])
+        vector.extend(reshaped)
+    return vector
+
+def vector_to_dictionary(vector, parameters):
+    """
+    converts parameter vector back into dictionary
+
+    Arguments:
+    vector: vector of parameters
+    paramters: dictionary of original model parameters
+
+    Return:
+    parameters: dictionary of parameters
+    """
+    params = {}
+    for i in parameters:
+        params[i] = vector[:parameters[i].shape[0]*parameters[i].shape[1]].reshape(parameters[i].shape)
+    return params
